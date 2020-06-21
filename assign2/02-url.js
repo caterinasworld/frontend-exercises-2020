235f49ea86c2dafca19d4d0ac83b529367af9b0c
Caterina Paun <caterina.paun@gmail.com>
2020-06-21 16:55:13 -0700

    Starter files for Assignment #2

---
+++ assign2/02-url.js
@@ -0,0 +1,24 @@
+// Enter your code here
+console.log('Enter your code here');
+
+// Input #1:
+// http://www.example.com
+
+// Output
+// http://www.example.com
+
+// Input #2:
+// http://www.example.com?name=r2d2
+
+// Output
+// http://www.example.com
+// name: r2d2
+
+// Input #3:
+// http://www.example.com?name=r2d2&email=r2d2%40me.com&human=no
+
+// Output
+// http://www.example.com
+// name: r2d2
+// email: r2d2@me.com
+// human: no
---
+++ assign2/07-characters.js
@@ -0,0 +1,4 @@
+// Enter your code here
+console.log('Enter your code here');
+
+const url = 'https://swapi.dev/api/people/';
